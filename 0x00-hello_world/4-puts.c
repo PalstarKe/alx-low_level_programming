3d720b0d1845aed8c19a1682048208ae47329b8e
PalstarKe <maishwriter2@gmail.com>
2022-09-08 02:25:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * return: always 0 (Success)
+ */
 int main(void)
 {
 
