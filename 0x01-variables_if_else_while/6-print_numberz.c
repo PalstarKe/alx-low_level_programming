34f18551f20957da12d76f764a898d5fb16de00c
PalstarKe <maishwriter2@gmail.com>
2022-09-10 07:22:30 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
