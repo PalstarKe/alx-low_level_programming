fe8832d5c1ae51a4c8687077416c50d92d2b9783
PalstarKe <111015374+PalstarKe@users.noreply.github.com>
2022-09-10 20:08:18 +0300

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,87 +1,45 @@
-nclude <stdio.h>
-
-
+#include <stdio.h>
 
 /**
+ * main - Prints numbers between 012 to 789.
  *
- *  * main - Prints numbers between 012 to 789.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int n, j, b;
 
-		int i, e, g;
+	n = 48;
+	j = 48;
+	b = 48;
 
-
-
-			i = 48;
-
-				e = 48;
-
-					g = 48;
-
-
-
-						while (e < 58)
-
-								{
-
-											i = 48;
-
-													while (i < 58)
-
-																{
-
-																				g = 48;
-
-																							while (g < 58)
-
-																											{
-
-																																if (e != i && e != g && i != g && e < i && i < g)
-
-																																					{
-
-																																											putchar(e);
-
-																																																putchar(i);
-
-																																																					putchar(g);
-
-																																																										if (i == 56 && e == 55 && g == 57)
-
-																																																																{
-
-																																																																							break;
-
-																																																																												}
-
-																																																															putchar(',');
-
-																																																																				putchar(' ');
-
-																																																																								}
-
-																																				g++;
-
-																																							}
-
-																										i++;
-
-																												}
-
-															e++;
-
-																}
-
-							putchar('\n');
-
-								return (0);
-
+	while (j < 58)
+	{
+		n = 48;
+		while (n < 58)
+		{
+			b = 48;
+			while (b < 58)
+			{
+				if (j != n && j != b && n != b && j < n && n < b)
+				{
+					putchar(j);
+					putchar(n);
+					putchar(b);
+					if (n == 56 && j == 55 && b == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				b++;
+			}
+			n++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
 }
